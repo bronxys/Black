a1dc14789f8fdae416515c447bf0119a0bdc0d74
matheuzinho <122645154+m4thxyz@users.noreply.github.com>
2024-03-01 14:52:46 -0300

    Add files via upload

---
+++ dono/info/infodono.js
@@ -0,0 +1,28 @@
+const infodono = (tempo, tempo_emoji, pushname, NickDono, nmrdn, NomeDoBot, prefix) => {
+return `
+${tempo} ${tempo_emoji}
+⏤͟͟͞͞ ꦿ${pushname}
+
+ㅤㅤㅤㅤI᩶N᩶F᩶O᩶R᩶M᩶A᩶ÇÕE᩶S᩶
+╭════════════════════╯
+ | × 𝙽𝙾𝙼𝙴 𝙳𝙾 𝙳𝙾𝙽𝙾 *↴*
+ |ㅤㅤ⇒${NickDono}
+ |
+ | × 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 *↴*
+ |ㅤㅤ⇒ wa.me/${nmrdn.split('@')[0]}
+ |
+ | × 𝙽𝙾𝙼𝙴 𝙳𝙾 𝙱𝙾𝚃 *↴*
+ |ㅤㅤ⇒${NomeDoBot}
+ |
+ | × 𝙿𝚁𝙴𝙵𝙸𝚇𝙾 𝚄𝚂𝙰𝙳𝙾 *↴*
+ |ㅤㅤ⇒( ${prefix} ) ❤️‍🔥
+ |
+╰════════════════════╮
+
+"𝘶𝘮 𝘴𝘢𝘣𝘪𝘰 𝘳𝘦𝘪 𝘯𝘶𝘯𝘤𝘢 𝘢𝘭𝘮𝘦𝘫𝘢 𝘶𝘮𝘢 𝘨𝘶𝘦𝘳𝘳𝘢, 𝘮𝘢𝘴 𝘱𝘢𝘴𝘴𝘢 𝘢 𝘷𝘪𝘥𝘢 𝘪𝘯𝘵𝘦𝘪𝘳𝘢 𝘴𝘦 𝘱𝘳𝘦𝘱𝘢𝘳𝘢𝘯𝘥𝘰 𝘱𝘳𝘢 𝘦𝘭𝘢" *~ Odin* 👑
+
+ꚠㅤ┋ㅤ@m4thxyz_ ヅ
+`
+}
+
+exports.infodono = infodono
